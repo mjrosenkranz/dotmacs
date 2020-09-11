15cf667cc110ea33fb7aa5242a2c68842214f2b3
morgan <rosenkranz1999@gmail.com>
2020-09-11 18:57:58 +0000

    starting to organize better and added custom functions

--- init.el
+++ init.el
@@ -25,19 +25,62 @@
 (eval-when-compile
   (require 'use-package))
 
-;; misc `emacs' settings
+;; `emacs' init settings
 (use-package emacs
   :init
-  (setq custom-file "~/.emacs.d/custom-settings.el")
-  (load custom-file t)
-  (setq ring-bell-function 'ignore)
+  (setq custom-file
+	"~/.emacs.d/custom-settings.el")
+  (load custom-file t))
+
+;; `emacs' gui settings
+(use-package emacs
+  :config
   (setq inhibit-startup-screen t)
   (tool-bar-mode -1)
   (blink-cursor-mode -1)
   (scroll-bar-mode -1)
+  (setq ring-bell-function 'ignore)
   (menu-bar-mode -1))
 
-;; setup ido
+;; buffer shit
+(use-package emacs
+  :config
+  (defun oct/kill-curr-buff (&optional arg)
+    "Kill the current buffer"
+    ;; make interactive, set arg to ???
+    (interactive "p")
+    ;; we can't kill a minibuffer so abort it
+    ;; otherwise kill the buffer
+    (if (minibufferp)
+	(abort-recursive-edit)
+      (kill-buffer (current-buffer)))
+    ;; if there are other windows we can kill this one
+    (when (and arg
+	       (not (one-window-p)))
+      (delete-window)))
+  :bind ("M-k" . oct/kill-curr-buff))
+
+;; ibuffer stuff
+(use-package ibuffer)
+
+;; get this dope theme
+(use-package modus-vivendi-theme
+  :ensure t
+  :config(load-theme 'modus-vivendi t))
+
+;; editor `emacs' settings
+(use-package emacs
+  :config
+  (defun oct/init ()
+    "Edit the `user-init-file' in the current buffer"
+    (interactive)
+    (find-file user-init-file))
+
+  (show-paren-mode 1)
+  (setq mouse-wheel-scroll-amount '(1 ((shift) . 1)))
+  (electric-pair-mode 1))
+
+
 (use-package icomplete
   :config
   (icomplete-mode))
@@ -46,7 +89,9 @@
 (use-package dired
   :defer
   :config
-  (setq dired-listing-switches "-aBhl  --group-directories-first"))
+  (setq dired-listing-switches
+	"-aBhl  --group-directories-first"))
+
 ;; make moving between and swapping windows easier
 (use-package windmove
   :defer
@@ -73,19 +118,20 @@
   (define-key minibuffer-local-must-match-map [escape] 'minibuffer-keyboard-quit)
   (define-key minibuffer-local-isearch-map [escape] 'minibuffer-keyboard-quit))
 
-;; get this dope theme
-(use-package modus-vivendi-theme
-  :ensure t
-  :config(load-theme 'modus-vivendi t))
 
 (use-package olivetti
   :ensure t)
+
+;; programming
+
+;; go
 
 (use-package go-mode
   :ensure t
   :defer t
   :config
   (setq gofmt-command "goimports")
+  (company-mode 1)
   (defun oct/go-mode-addons ()
     (add-hook 'before-save-hook 'gofmt nil 'local))
   (add-hook 'go-mode-hook 'oct/go-mode-addons))
@@ -94,8 +140,5 @@
 	      (set (make-local-variable 'company-backends) '(company-go))))
 
 
-(use-package company :ensure t
-  :config
-  (company-mode))
+(use-package company :ensure t :config (company-mode 1))
 (use-package company-go :ensure t)
-
