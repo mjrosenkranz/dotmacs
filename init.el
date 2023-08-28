499ef5968698fdfadebd4b6d2212aa7c57b1f6c2
Morgan Rosenkranz <rosenkranz1999@gmail.com>
2023-08-27 23:32:19 -0400

    added ivy, much needed

--- init.el
+++ init.el
@@ -46,6 +46,28 @@
   ;; Initialize.
   (setq which-key-mode-idle-delay 1))
 
+(use-package ivy
+  ;; TODO: do we need this at startup?
+  :demand
+  :diminish
+  ;; TODO: move these to general?
+  :bind (("C-s" . swiper)
+         :map ivy-minibuffer-map
+         ("TAB" . ivy-alt-done)
+         ("C-l" . ivy-alt-done)
+         ("C-j" . ivy-next-line)
+         ("C-k" . ivy-previous-line)
+         :map ivy-switch-buffer-map
+         ("C-k" . ivy-previous-line)
+         ("C-l" . ivy-done)
+         ("C-d" . ivy-switch-buffer-kill)
+         :map ivy-reverse-i-search-map
+         ("C-k" . ivy-previous-line)
+         ("C-d" . ivy-reverse-i-search-kill))
+  :config
+  (ivy-mode 1))
+  
+
 
 ;; ---settings---
 
