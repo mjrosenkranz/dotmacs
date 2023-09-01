d877b8a8ba428f57d5c7a80d50f19d8309f9b2ed
Morgan Rosenkranz <rosenkranz1999@gmail.com>
2023-09-01 08:17:39 -0400

    added rust and nix config

--- init.el
+++ init.el
@@ -66,6 +66,19 @@
   ; :hook (lsp-mode . efs/lsp-mode-setup)
   :init
   (setq lsp-keymap-prefix "C-c l")  ;; Or 'C-l', 's-l'
+  :custom
+  (lsp-rust-analyzer-cargo-watch-command "clippy")
+  (lsp-eldoc-render-all t)
+  (lsp-idle-delay 0.6)
+  ;; enable / disable the hints as you prefer:
+  (lsp-inlay-hint-enable t)
+  ;; These are optional configurations. See https://emacs-lsp.github.io/lsp-mode/page/lsp-rust-analyzer/#lsp-rust-analyzer-display-chaining-hints for a full list
+  (lsp-rust-analyzer-display-lifetime-elision-hints-enable "skip_trivial")
+  (lsp-rust-analyzer-display-chaining-hints t)
+  (lsp-rust-analyzer-display-lifetime-elision-hints-use-parameter-names nil)
+  (lsp-rust-analyzer-display-closure-return-type-hints t)
+  (lsp-rust-analyzer-display-parameter-hints nil)
+  (lsp-rust-analyzer-display-reborrow-hints nil)
   :config
   (lsp-enable-which-key-integration t))
 
@@ -105,6 +118,13 @@
   (setq typescript-indent-level 4))
 
 ;; rust
+
+(use-package rustic
+  :ensure)
+
+;; nix
+(use-package nix-mode
+  :mode "\\.nix\\'")
 
 ;; completion
 
