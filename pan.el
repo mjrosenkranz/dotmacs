9a041d69573f84cc00fbab510dc1871b3c7363dd
mkr <rosenkranz1999@gmail.com>
2023-11-11 15:31:52 -0500

    more planning work on panel

--- pan.el
+++ pan.el
@@ -5,4 +5,41 @@
   (if panel-mode
       (message "panel-mode2 turned on!")
       (message "panel-mode2 turned off!")))
+
   
+
+(defun panel-mode--init ()
+  ;; add hooks for every time a window is created, deleted, moved
+  ;; or a frame is created, delted
+
+  (panel-mode--init-advice)
+  )
+
+
+(defun panel-mode--deinit ()
+  )
+
+;; hook shit
+;; after-frame-functions
+;; delete-frame-functions
+;; kill-buffer-query-functions
+
+
+;; advice shit
+          ;; (ad-activate 'switch-to-buffer)
+          ;; (ad-activate 'display-buffer)
+          ;; (ad-activate 'set-window-buffer)
+          ;; (ad-activate 'switch-to-prev-buffer)
+          ;; (ad-activate 'recursive-edit)
+          ;; (ad-activate 'exit-recursive-edit)
+
+(defun panel-mode--init-advice ()
+    (ad-activate 'switch-to-buffer)
+  )
+
+(current-window-configuration)
+#<window-configuration>
+
+(setq panel-current-window-configuration (current-window-configuration))
+
+(set-window-configuration panel-current-window-configuration)
